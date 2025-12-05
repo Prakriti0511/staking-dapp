91cc0e5580ea1c7396f33073e10869b7bfce5199
Prakriti0511 <prakriti0511@gmail.com>
2025-12-05 21:38:05 +0530

    ui imprved and bugs fixed

--- Components/Admin/AdminCard.jsx
+++ Components/Admin/AdminCard.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const AdminCard = ({name, value}) => {
   return (
   <div className="col-12 col-md-4">
-    <div className="stats">
-      <span className="stats__value">{value}</span>
-      <p className={"stats__name"}>{name}</p>
+    <div className="stats ">
+      <span className="stats__value" style={{display: 'block', textAlign: 'center'}}>{value}</span>
+      <p className={"stats__name"} style={{textAlign: 'center'}}>{name}</p>
     </div>
   </div>)
 };
