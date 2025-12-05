7953416b2e9c576a6d4d0977f05999eb308f35b8
Prakriti0511 <prakriti0511@gmail.com>
2025-12-05 20:56:22 +0530

    title showing correctly now

--- Components/Admin/RegularComp/Title.jsx
+++ Components/Admin/RegularComp/Title.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Title = () => {
-  return <div>Title</div>;
+const Title = ({ title }) => {
+  return (
+    <div className="col-12 mt-3">
+      <h5 style={{ color: "#fff", marginBottom: "20px" }}>{title}</h5>
+    </div>
+  );
 };
 
 export default Title;
